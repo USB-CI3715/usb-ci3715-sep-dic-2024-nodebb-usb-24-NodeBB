bdb52660c6b00e7bf4bb73ed7d5a4559ed2e8fa3
Jeamhowards Montiel <19-10234@usb.ve>
2024-10-29 16:35:22 -0400

    Add upgrade scripts to create 'Profesores' and 'Estudiantes' groups

---
+++ src/upgrades/3.8.5/professors_groups.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+    name: 'Creating Profesores group',
+    timestamp: Date.UTC(2024, 10, 28), 
+    method: async function () {
+        const groups = require('../../groups');
+        const exists = await groups.exists('Profesores');
+        if (exists) {
+            return;
+        }
+        await groups.create({
+            name: 'Profesores',
+            userTitle: 'Profesores',
+            description: 'Grupo para profesores',
+            hidden: 0,
+            private: 1,
+            disableJoinRequests: 1,
+        });
+        await groups.show('Profesores');
+    },
+};
---
+++ src/upgrades/3.8.5/students_group.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+    name: 'Creating Estudiantes group',
+    timestamp: Date.UTC(2024, 10, 28), 
+    method: async function () {
+        const groups = require('../../groups');
+        const exists = await groups.exists('Estudiantes');
+        if (exists) {
+            return;
+        }
+        await groups.create({
+            name: 'Estudiantes',
+            userTitle: 'Estudiantes',
+            description: 'Grupo para Estudiantes',
+            hidden: 0,
+            private: 1,
+            disableJoinRequests: 1,
+        });
+        await groups.show('Estudiantes');
+    },
+};
