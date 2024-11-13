a218d90128b023206406380e6a7aa90b1c1e5e34
Jhonaiker Blanco <blancojhonaiker@gmail.com>
2024-11-12 22:38:29 -0400

    Tests de creacion de posts corriendo

--- src/posts/create.js
+++ src/posts/create.js
@@ -19,7 +19,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
-		const isUrgent = data.isUrgent || false;
+		let isUrgent = Boolean(data.isUrgent) || false;
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
